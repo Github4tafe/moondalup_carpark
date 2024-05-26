03a3c88424ab3561dfeea8585b130ee8f19920c6
20124966 <20124966@tafe.wa.edu.au>
2024-05-27 00:48:33 +0800

    Added constructors and attributes to the car park class

--- src/car_park.py
+++ src/car_park.py
@@ -5,7 +5,7 @@ class CarPark:
         self.capacity = capacity
         self.sensors = sensors or []  # uses the first value if not None, otherwise uses the second value
         self.plates = plates or []
-        self.displays = displays
+        self.displays = displays or []
 
     def __str__(self):
         carpark_info = f"Car Park at {location}, with {capacity}"
--- src/display.py
+++ src/display.py
@@ -1,3 +1,11 @@
 
 class Display:
-    pass # this just means nothing... it actually does nothing
+    def __init__(self, id, message="", is_on=False, car_park="Unknown"):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):
+        carpark_info = f"Car Park at {location}, with {capacity}"
+        return carpark_info
