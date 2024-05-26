6cee5e4ee978b6177840c28c8938dc8fa5b91e32
20124966 <20124966@tafe.wa.edu.au>
2024-05-27 00:58:14 +0800

    Added constructors and attributes to the display and sensor classes

--- src/sensor.py
+++ src/sensor.py
@@ -1,3 +1,24 @@
 
 class Sensor:
-    pass # this just means nothing... it actually does nothing
+    def __init__(self, id, is_active=False, car_park="Unknown"):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        sensor_info = f"Sensor of {id} at {car_park} with activity status of {is_active}"
+        return sensor_info
+
+
+class EntrySensor(Sensor):
+    def __init__(self, id, is_active=False, car_park="Unknown"):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+
+class ExitSensor(Sensor):
+    def __init__(self, id, is_active=False, car_park="Unknown"):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
